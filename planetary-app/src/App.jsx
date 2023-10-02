19ce65d7e66113551f5aa2f1d9ea9e33d8d5a172
Jakub Janicki <jjanicki08@gmail.com>
2023-10-03 00:01:07 +0200

    Initial Planet App

---
+++ planetary-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="main-wrapper">test</div>
+    </Fragment>
+  );
+}
+
+export default App;
